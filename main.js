4fb26c9fb45191d5fb6eb689c034e0a5192a63bd
Jezabel <jezabel79@hotmail.es>
2023-04-12 11:02:00 +0200

    añadido inputs y javascript

---
+++ main.js
@@ -0,0 +1,19 @@
+function aleatorio() {
+let select = document.getElementById("list");
+let items = select.getElementByTagName("option");
+let index = Math.floor(Math.random()*items.length);
+items[index].setAttribute('selected', true);
+}
+ function agregar() {
+    if (document.frm.txtNombre.value.length == 0) {
+      alert("No se permiten textos vacios")
+    } else {
+      var nuevoitem = document.frm.txtNombre.value;
+      var list = document.getElementById("selNombre");
+      var option = document.createElement("option");
+      list.options.add(option, 0);
+      list.options[0].value = nuevoitem;
+      list.options[0].innerText = nuevoitem;
+      document.frm.txtNombre.value = "";
+    }
+  }
