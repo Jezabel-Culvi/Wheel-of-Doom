a721a852af81fd1200c5908f0e356d46ee8192fa
silvanacinthia <javivita1986@gmail.com>
2023-04-14 11:51:26 +0200

    ärreglado modo noche

--- main.js
+++ main.js
@@ -1,27 +1,25 @@
 function aleatorio() {
-let select = document.getElementById("list");
-let items = select.getElementByTagName("option");
-let index = Math.floor(Math.random()*items.length);
-items[index].setAttribute('selected', true);
+  let select = document.getElementById("list");
+  let items = select.getElementByTagName("option");
+  let index = Math.floor(Math.random() * items.length);
+  items[index].setAttribute('selected', true);
 }
- function agregar() {
-    if (document.frm.txtNombre.value.length == 0) {
-      alert("No se permiten textos vacios")
-    } else {
-      var nuevoitem = document.frm.txtNombre.value;
-      var list = document.getElementById("selNombre");
-      var option = document.createElement("option");
-      list.options.add(option, 0);
-      list.options[0].value = nuevoitem;
-      list.options[0].innerText = nuevoitem;
-      document.frm.txtNombre.value = "";
-    }
-  
+function agregar() {
+  if (document.frm.txtNombre.value.length == 0) {
+    alert("No se permiten textos vacios")
+  } else {
+    var nuevoitem = document.frm.txtNombre.value;
+    var list = document.getElementById("selNombre");
+    var option = document.createElement("option");
+    list.options.add(option, 0);
+    list.options[0].value = nuevoitem;
+    list.options[0].innerText = nuevoitem;
+    document.frm.txtNombre.value = "";
   }
 
+}
 
-
-
-
-  //añadir codigo modo nocturno
-  
+function enableDarkMode() {
+  let main_body = document.body;
+  main_body.classList.toggle("dark-mode");
+}
